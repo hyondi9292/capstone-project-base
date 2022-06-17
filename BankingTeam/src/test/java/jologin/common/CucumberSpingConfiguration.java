59a51f90f869689e83213af8a66449832bb1df79
김우식 <48271302+alphamega89@users.noreply.github.com>
2022-06-17 10:18:03 +0900

    Delete BankingTeam directory

--- BankingTeam/src/test/java/jologin/common/CucumberSpingConfiguration.java
+++
@@ -1,9 +0,0 @@
-package jologin.common;
-
-import io.cucumber.spring.CucumberContextConfiguration;
-import jologin.BankingTeamApplication;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@CucumberContextConfiguration
-@SpringBootTest(classes = { BankingTeamApplication.class })
-public class CucumberSpingConfiguration {}
