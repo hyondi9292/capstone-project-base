0775002f37e39ab8cceabafa5a61b3a25b1a84d4
김우식 <48271302+alphamega89@users.noreply.github.com>
2022-06-17 10:18:30 +0900

    Delete DepositTeam directory

--- DepositTeam/src/test/java/jologin/common/CucumberSpingConfiguration.java
+++
@@ -1,9 +0,0 @@
-package jologin.common;
-
-import io.cucumber.spring.CucumberContextConfiguration;
-import jologin.DepositTeamApplication;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@CucumberContextConfiguration
-@SpringBootTest(classes = { DepositTeamApplication.class })
-public class CucumberSpingConfiguration {}
