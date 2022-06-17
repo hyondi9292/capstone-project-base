935d18d83d196c7a418fe478fa17f06bccdf2e71
ANJOON_JEONG <jaj1012@naver.com>
2022-06-17 05:31:58 +0000

    commit on main

--- CustomerTeam/src/main/java/msalogin/domain/CustomerRegistered.java
+++ CustomerTeam/src/main/java/msalogin/domain/CustomerRegistered.java
@@ -5,10 +5,19 @@ import lombok.Data;
 import msalogin.domain.*;
 import msalogin.infra.AbstractEvent;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "CustomerList_Base")
 @Data
 public class CustomerRegistered extends AbstractEvent {
 
+    @Id
     private Long customerId;
+    @Column
     private String status;
 
     public CustomerRegistered() {
