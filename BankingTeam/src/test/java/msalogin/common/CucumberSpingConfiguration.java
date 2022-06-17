90c0db5811ee90ec6cfde04b65b778c1b0351a02
김우식 <48271302+alphamega89@users.noreply.github.com>
2022-06-17 16:07:01 +0900

    Delete BankingTeam directory

--- BankingTeam/src/test/java/msalogin/common/CucumberSpingConfiguration.java
+++
@@ -1,9 +0,0 @@
-package msalogin.common;
-
-import io.cucumber.spring.CucumberContextConfiguration;
-import msalogin.BankingTeamApplication;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@CucumberContextConfiguration
-@SpringBootTest(classes = { BankingTeamApplication.class })
-public class CucumberSpingConfiguration {}
