f63c298ebb1d983d1239cd240f2aa9d9cb89473d
김우식 <48271302+alphamega89@users.noreply.github.com>
2022-06-22 16:57:55 +0900

    Delete CustomerTeam directory

--- CustomerTeam/src/main/java/msalogin/domain/CustomerService.java
+++
@@ -1,8 +0,0 @@
-package msalogin.domain;
-
-public interface CustomerService {
-    void createCustomer (String customerId, String Address, String Telno, String Name, String JuminNo);
-    void deleteCustomer (String customerId);
-    Customer getCustomer(String customerId);
-    Customer save(String data);
-}
--- CustomerTeam/src/test/java/msalogin/common/CucumberSpingConfiguration.java
+++
@@ -1,9 +0,0 @@
-package msalogin.common;
-
-import io.cucumber.spring.CucumberContextConfiguration;
-import msalogin.CustomerTeamApplication;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@CucumberContextConfiguration
-@SpringBootTest(classes = { CustomerTeamApplication.class })
-public class CucumberSpingConfiguration {}
