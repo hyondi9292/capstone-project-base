06c31e307dc8b5d9b1855c7c653137a1ddf9c6bd
김우식 <48271302+alphamega89@users.noreply.github.com>
2022-06-22 16:58:17 +0900

    Delete Login directory

--- Login/src/main/java/msalogin/domain/LoginRepository.java
+++
@@ -1,9 +0,0 @@
-package msalogin.domain;
-
-import msalogin.domain.*;
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource(collectionResourceRel = "logins", path = "logins")
-public interface LoginRepository
-    extends PagingAndSortingRepository<Login, Long> {}
--- Login/src/test/java/msalogin/common/CucumberSpingConfiguration.java
+++
@@ -1,9 +0,0 @@
-package msalogin.common;
-
-import io.cucumber.spring.CucumberContextConfiguration;
-import msalogin.LoginApplication;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@CucumberContextConfiguration
-@SpringBootTest(classes = { LoginApplication.class })
-public class CucumberSpingConfiguration {}
